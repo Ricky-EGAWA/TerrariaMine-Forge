c6ac83b018b581fa48f43759bd8b770305f6e732
rickyega <rickyega@yahoo.ne.jp>
2025-01-04 16:48:05 +0100

    fix giant vindicator

--- src/main/java/com/ricky/xp_giant_mod/entity/client/GiantVindicatorRenderer.java
+++ src/main/java/com/ricky/xp_giant_mod/entity/client/GiantVindicatorRenderer.java
@@ -2,6 +2,7 @@ package com.ricky.xp_giant_mod.entity.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.ricky.xp_giant_mod.entity.custom.GiantVindicator;
+import com.ricky.xp_giant_mod.event.ChargeJumpHandler;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.VindicatorRenderer;
 import net.minecraft.world.entity.monster.Vindicator;
@@ -15,7 +16,11 @@ public class GiantVindicatorRenderer extends VindicatorRenderer {
     protected void scale(Vindicator entity, PoseStack poseStack, float partialTicks) {
         if (entity instanceof GiantVindicator) {
             float scaleFactor = 6;
-            poseStack.scale(scaleFactor, scaleFactor, scaleFactor);
+            if (ChargeJumpHandler.isMobFlattened(entity)){
+                poseStack.scale(scaleFactor, scaleFactor/8, scaleFactor);
+            }else{
+                poseStack.scale(scaleFactor, scaleFactor, scaleFactor);
+            }
         }
         super.scale(entity, poseStack, partialTicks);
     }
