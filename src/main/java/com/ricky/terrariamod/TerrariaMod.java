01f024df23dd2c9ca02e580604d1349ad3606eb3
rickyega <rickyega@yahoo.ne.jp>
2025-01-20 19:20:18 +0100

    add block

--- src/main/java/com/ricky/terrariamod/TerrariaMod.java
+++ src/main/java/com/ricky/terrariamod/TerrariaMod.java
@@ -1,6 +1,7 @@
 package com.ricky.terrariamod;
 
 import com.mojang.logging.LogUtils;
+import com.ricky.terrariamod.block.ModBlocks;
 import com.ricky.terrariamod.item.ModCreativeModTabs;
 import com.ricky.terrariamod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
@@ -26,6 +27,7 @@ public class TerrariaMod{
 
         ModCreativeModTabs.register(modEventBus);
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
